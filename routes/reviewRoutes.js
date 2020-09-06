14e127f08246b6c9bd59b264ea61923973f1a2f2
Siddhant Mittal <sidmit2001@gmail.com>
2020-09-06 19:41:31 +0530

    Add files via upload

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,14 @@
+const reviewController = require('./../controllers/reviewController');
+const express = require('express');
+const router = express.Router({
+    mergeParams: true
+});
+const authController = require('./../controllers/authController');
+
+router.use(authController.protect);
+
+router.route('/').get(reviewController.getAllReviews).post(authController.restrictTo('user'), reviewController.setTourUserIds, reviewController.createReview);
+
+router.route('/:id').get(reviewController.getReview).delete(authController.restrictTo('user', 'admin'), reviewController.deleteReview).patch(authController.restrictTo('user', 'admin'), reviewController.updateReview);
+
+module.exports = router;
